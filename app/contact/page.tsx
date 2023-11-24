4076b3d63b8d3e6cdaea2716ce36cceb5a231e7b
Gauthier <gauthierdaurat1@gmail.com>
2023-11-24 12:17:11 +0100

    Ajout du formulaire de contact (non fonctionnel)

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,26 @@
-export default function ContactPage(){
-    return <p>Je suis la page contact</p>
-};
+import React from 'react';
+
+export default function ContactPage() {
+  return (
+    <form className="container">
+      <h1>Contactez-nous !</h1>
+      <div className="email block">
+        <label htmlFor="frm-email">Email</label>
+        <input
+          id="frm-email"
+          type="email"
+          name="email"
+          autoComplete="email"
+          required
+        />
+      </div>
+      <div className="message block">
+        <label htmlFor="frm-message">Message</label>
+        <textarea id="frm-message" rows={6} name="message"></textarea>
+      </div>
+      <div className="button block">
+        <button type="submit">Envoyer</button>
+      </div>
+    </form>
+  );
+}
