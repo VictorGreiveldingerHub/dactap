2a51d129121d75a4139224ee5aba12f2bf8d6dca
LeBuffaloGrille <91696201+LeBuffaloGrille@users.noreply.github.com>
2023-12-05 09:47:50 +0000

    Mise en place de la fonctionnalité disconnection dans le Header

--- app/components/header/pages.tsx
+++ app/components/header/pages.tsx
@@ -1,84 +1,90 @@
 "use client";
 
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Settings } from "lucide-react";
-import DactapSvg from "@/app/ui/utils/SvgComponents/DactapSvg"
+import DactapSvg from "@/app/ui/utils/SvgComponents/DactapSvg";
+import { signOut, useSession } from "next-auth/react";
 
 const navLinks = [
-    {
-        key: "dactap",
-        name: <DactapSvg />,
-        href: "/"
-    },
-    {
-        key: "jeux",
-        name: "Jeux",
-        href: "/games"
-    },
-    {
-        key: "profil",
-        name: "Profil",
-        href: "/profil"
-    },
-    {
-        key: "statistiques",
-        name: "Statistiques",
-        href: "/dashboard"
-    },
-]
+  {
+    key: "dactap",
+    name: <DactapSvg />,
+    href: "/",
+  },
+  {
+    key: "jeux",
+    name: "Jeux",
+    href: "/games",
+  },
+  {
+    key: "profil",
+    name: "Profil",
+    href: "/profil",
+  },
+  {
+    key: "statistiques",
+    name: "Statistiques",
+    href: "/dashboard",
+  },
+];
 
 const Header = () => {
+  const pathname = usePathname();
+  let isLogged = false;
+  const { status, data } = useSession();
+  const [loading, setLoading] = useState(true);
 
-    const pathname = usePathname();
-    const [isLogged, SetLogged] = useState(false);
-    
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      if (status === "unauthenticated") {
+        setLoading(false);
+      } else if (status === "authenticated") {
+        setLoading(false);
+      }
+    };
 
-    return (
-        <header className="flex mx-10 my-5 font-bold">
-            <nav className="flex basis-1/2 justify-between items-end">
-                {navLinks.map((link) => {
-                    const isActive = pathname.startsWith(link.href);
-                    return (
-                        <Link 
-                            key={link.key} 
-                            href={link.href}
-                            className={isActive ? "underline underline-offset-4" : ""}
-                        >
-                            {link.name}
-                        </Link>
+    checkAuthentication();
+  }, [status]);
 
-                    )
-                    })
-                }
-            <nav className="flex basis-1/2 justify-between items-center">
-                <Link href="/" className="text-4xl">Dactap</Link>
-                <Link href="/games" className={`mr-4 my-auto hover:text-indigo-600 font-normal font-serif text-xl`}>Jeux</Link>
-                <Link href="/profil">Profil</Link>
-                <Link href="/dashboard">Statistiques</Link>
-                <Link href="/connection">Connexion</Link>
-            </nav>
-            <div className="flex basis-1/2 justify-center items-end">
-                {isLogged ? (
-                    <button>Déconnexion</button>
-                ) : (
-                    <>
-                        <button>
-                            <Link href="/signin">Connexion</Link>
-                        </button>
-                        <button className="px-3 shadow-md border border-blue-900 rounded-full hover:bg-blue-900 hover:text-blue-100">
-                            <Link href="/signup">Inscription</Link>
-                        </button>
-                    </>
-                )}
-            </div>
-
-            <button >
-                <Settings />
+  return (
+    <header className="flex mx-10 my-5 font-bold">
+      <nav className="flex basis-1/2 justify-between items-end">
+        {navLinks.map((link) => {
+          const isActive = pathname.startsWith(link.href);
+          return (
+            <Link
+              key={link.key}
+              href={link.href}
+              className={isActive ? "underline underline-offset-4" : ""}
+            >
+              {link.name}
+            </Link>
+          );
+        })}
+      </nav>
+      <div className="flex basis-1/2 justify-center items-end">
+        {status === "authenticated" && (
+          <button onClick={() => signOut()}>Déconnexion</button>
+        )}
+        {status === "unauthenticated" && (
+          <>
+            <button>
+              <Link href="/signin">Connexion</Link>
             </button>
-        </header>
-    );
+            <button className="px-3 shadow-md border border-blue-900 rounded-full hover:bg-blue-900 hover:text-blue-100">
+              <Link href="/signup">Inscription</Link>
+            </button>
+          </>
+        )}
+      </div>
+
+      <button>
+        <Settings />
+      </button>
+    </header>
+  );
 };
 
 export default Header;
