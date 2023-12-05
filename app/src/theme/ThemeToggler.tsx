8b6b5f905240a2b69bef1754d075eb26e9ce5218
Victor G <victorgreiveldinger@gmail.com>
2023-12-05 07:41:08 +0100

    Conditionnelle bouton connexion / deconnexion

--- app/src/theme/ThemeToggler.tsx
+++ app/src/theme/ThemeToggler.tsx
@@ -2,7 +2,11 @@
 
 import { useState, useEffect } from 'react';
 import { useTheme } from "next-themes";
-import { SunMedium, Moon } from 'lucide-react';
+import { 
+    SunMedium, 
+    Moon, 
+    SunMoon, 
+} from 'lucide-react';
 
 
 const ThemeToggler : React.FC = () => {
@@ -19,15 +23,28 @@ const ThemeToggler : React.FC = () => {
 
     return (
         <div>
-            <button onClick={() => setTheme("system")}>System</button> {/* Si présent afficher la possibilité d'utiliser le système par défaut de l'utilisateur */}
-            <button 
+            {/* <button onClick={() => setTheme("system")}>
+                <SunMoon size={20}/>
+            </button> Si présent afficher la possibilité d'utiliser le système par défaut de l'utilisateur */}
+            {/* <button 
                 onClick={() => {
                     setTheme(theme === "light" ? "dark" : "light")
                 }}
             >
                 <SunMedium size={20} />
                 <Moon size={20} />
-            </button>
+            </button> */}
+            <fieldset>
+                {/* <legend className="sr-only">
+                    Thèmes
+                </legend> */}
+                <div className="border border-black rounded p-4">
+                    <input id="theme_system" className="border border-black rounded p-4" type="radio" name="system" value="system" />
+                    <label htmlFor="theme_system" className="border border-black rounded p-4">
+                        <SunMoon size={20}/> Par défaut
+                    </label>
+                </div>
+            </fieldset>
         </div>
     );
 };
